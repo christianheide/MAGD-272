f0ea82a35f220fa943d8e401ba2ad64971559a31
christianheide <heidecr23@uww.edu>
2016-02-07 15:37:15 -0600

    Added Assignment 2 to Master
    I committed the assignment to the master file

---
+++ assignment 2/Assets/sceneChange.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sceneChange : MonoBehaviour {
+	public string highScore;
+	public string gameScene;
+	void Start(){
+
+	}
+	void Update(){
+
+	}
+	public void loadHighScore(){
+		SceneManager.LoadScene (highScore, LoadSceneMode.Single);
+	}
+	public void loadGameScene(){
+		SceneManager.LoadScene (gameScene, LoadSceneMode.Single);
+	}
+}
