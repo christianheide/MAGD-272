41a1c13834db3254f352f4407da748ebbccca0b1
christianheide <heidecr23@uww.edu>
2016-02-15 01:31:18 -0600

    Put assignment 3 into github
    committed assignment 3 to repo

---
+++ assignment 3/Assets/Scripts/noDestroy.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class noDestroy : MonoBehaviour {
+
+	void Awake(){
+		DontDestroyOnLoad (gameObject);
+	}
+}
---
+++ assignment 3/Assets/Scripts/sceneChange.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sceneChange : MonoBehaviour {
+	public string gameScene;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void loadGameScene(){
+		SceneManager.LoadScene (gameScene, LoadSceneMode.Single);
+	}
+}
